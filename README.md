efbde8557324105ba72b602ca4c125e6c06f8916
pandas9 <81386636+pandas9@users.noreply.github.com>
2022-01-02 07:55:10 -0800

    v0.1

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# auto-art-gen
+# Auto Art Gen with <a href="https://github.com/sepandhaghighi/samila">Samila</a>
+
+Generates random art based on random points on Cartesian coordinate system calculated from random set of mathemtical formulas with random parameter.
+
+## Requirements
+
+```pip install -r requirements.txt```
+```python>=3```
+```samila>=0.3```
+
+If requirements.txt won't install check <a href="https://github.com/sepandhaghighi/samila#installation">Samila installation</a>
+
+## Usage
+
+Every color, projection and point is randomly selected.
+
+```python3 main.py```
+
+```main.py```
+```
+if __name__ == '__main__':
+    Generate(num_pieces=300)
+```
+
+![example-1](examples/218.png?raw=true)
+![example-2](examples/271.png?raw=true)
+![example-3](examples/300.png?raw=true)
+
+## References
+https://github.com/sepandhaghighi/samila <br />
+https://github.com/cutterkom/generativeart <br />
---
+++ main.py
@@ -0,0 +1,74 @@
+import random
+
+import operator
+import math
+import matplotlib.pyplot as plt
+
+from samila import GenerativeImage, Projection, VALID_COLORS
+
+import os
+import sys
+
+class Generate:
+    def __init__(self, num_pieces, bg_color="black"):
+        projections = [ Projection.POLAR, Projection.AITOFF, Projection.LAMBERT, Projection.MOLLWEIDE ]
+        f1, f2 = self.algo_functions()
+
+        self.dir_path = os.path.dirname(os.path.abspath(__file__))
+        os.makedirs(f'{self.dir_path}/output', exist_ok=True)
+
+        for i in range(num_pieces):
+            self.g = GenerativeImage(f1, f2)
+            self.g.generate()
+            self.g.plot(color=random.choice(VALID_COLORS), bgcolor=bg_color, projection=random.choice(projections))
+            self.save(i +1)
+
+    def f_pow(self, a, b):
+        try:
+            return a ** b
+        except:
+            return a * b
+
+    def algo_functions(self):
+        math_funcs = [
+            math.cos,
+            math.sin,
+            math.tan,
+            math.asinh,
+            math.cosh,
+            math.sinh,
+            math.tanh,
+            abs
+        ]
+
+        operators = {
+            '+': operator.add,
+            '-': operator.sub,
+            '*': operator.mul,
+            '**': self.f_pow
+        }
+
+        def func(x, y):
+            result = random.uniform(-3,3)
+            for n in range(0, random.randint(3, 9)):
+                if n == 0:
+                    result = operators[random.choice(list(operators.keys()))](
+                        random.choice(math_funcs)(result),
+                        random.uniform(-3,3)
+                    )
+                else:
+                    result = operators[random.choice(list(operators.keys()))](
+                        random.choice(math_funcs)(random.choice([x, y])),
+                        random.choice([x, y, result])
+                    )
+
+            return result
+
+        return func, func
+
+    def save(self, filename):
+        self.g.save_data(file_adr=f'{self.dir_path}/output/{filename}.json')
+        plt.savefig(f'{self.dir_path}/output/{filename}.png')
+
+if __name__ == '__main__':
+    Generate(num_pieces=300)
